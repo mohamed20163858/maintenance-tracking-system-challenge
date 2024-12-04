b982cbe2ba3869c9f537c895e95a41c6d7c2232e
Mohamed Saleh <mohamed20163858@gmail.com>
2024-12-04 20:42:28 +0200

    Add pagination function to equipment table

--- app/components/EquipmentTable.tsx
+++ app/components/EquipmentTable.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect, useReducer } from "react";
 import {
   useReactTable,
   flexRender,
@@ -29,6 +29,7 @@ const EquipmentTable: React.FC<{ data: Equipment[] }> = ({ data }) => {
     "All" | "Operational" | "Down" | "Maintenance" | "Retired"
   >("All");
   const [sorting, setSorting] = useState<SortingState>([]);
+  //   const rerender = useReducer(() => ({}), {})[1]
 
   // Column helper for creating columns
   const columnHelper = createColumnHelper<Equipment>();
@@ -105,6 +106,7 @@ const EquipmentTable: React.FC<{ data: Equipment[] }> = ({ data }) => {
     },
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
     getFilteredRowModel: getFilteredRowModel(), //client side filtering
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
@@ -231,6 +233,71 @@ const EquipmentTable: React.FC<{ data: Equipment[] }> = ({ data }) => {
           ))}
         </tbody>
       </table>
+      <div className="h-5" />
+      <div className="flex items-center justify-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            min="1"
+            max={table.getPageCount()}
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+        >
+          {[5, 10, 15, 20, 25].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      {/* <div>{table.getPrePaginationRowModel().rows.length} Rows</div> */}
     </div>
   );
 };
